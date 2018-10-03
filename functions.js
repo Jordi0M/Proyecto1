f623caf34b3b29f35c5dd4fbf80b0616d5098d0f
JoseLomero <jlomerooro@iesesteveterradas.cat>
2018-10-03 17:24:25 +0200

    css cambiado

--- functions.js
+++ functions.js
@@ -1,3 +1,5 @@
-function myFunction() {
-    document.getElementById("myForm").reset();
+function contador() {
+	counter = 0;
+	document.getElementsByClassName('contador1').innerHTML =document.getElementsByClassName('contador1').innerHTML + counter;
+	counter+=1;
 }
